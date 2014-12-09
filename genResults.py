b439173eacc47033a5b9132745f2789e266d315c
Arjun Mukhopadhyay <arjunmukhopadhyay11724@gmail.com>
2014-12-09 09:00:45 +0530

    Read Paths from Config
    ConfigParser change

--- genResults.py
+++ genResults.py
@@ -5,13 +5,34 @@ from mechanize import Browser
 from mechanize import Browser
 from datetime import datetime
 import threading
+import ConfigParser
 from threading import Thread
 from Queue import Queue
-# from mechanize import
 
-# var = raw_input("Please enter word: ")
-# generateReport(var)
-#
+def ConfigSectionMap(section):
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+
+Config = ConfigParser.ConfigParser()
+Config.read("config.ini")
+print Config.sections()
+
+x= ConfigSectionMap('Paths')
+
+path = ConfigSectionMap("Paths")['path']
+wordSentence = ConfigSectionMap("Paths")['wordsentences']
+wordDefn = ConfigSectionMap("Paths")['worddefn']
+wordDefnSentence = ConfigSectionMap("Paths")['worddefnsentence']
+wordCovered = ConfigSectionMap("Paths")['wordcovered']
 
 def actualCalling(q):
   while True:
@@ -21,15 +42,6 @@ def actualCalling(q):
     generateReport(var, wordDefn, wordSentence, wordDefnSentence, wordCovered)
     q.task_done()
 
-path = "C:\\Users\\amukhopadhyay\\Downloads\\TrickyStuff\\PythonGrey\\wordsDontKnow.txt"
-# wordSentence ="C:\\Users\\amukhopadhyay\\Downloads\\TrickyStuff\\PythonGrey\\WordSentence.csv"
-# wordDefn ="C:\\Users\\amukhopadhyay\\Downloads\\TrickyStuff\\PythonGrey\\WordDefinition.csv"
-# wordDefnSentence ="C:\\Users\\amukhopadhyay\\Downloads\\TrickyStuff\\PythonGrey\\WordDefinitionSentence.csv"
-wordSentence ="C:\\Users\\amukhopadhyay\\Desktop\\WordSentence.csv"
-wordDefn ="C:\\Users\\amukhopadhyay\\Desktop\\WordDefinition.csv"
-wordDefnSentence ="C:\\Users\\amukhopadhyay\\Desktop\\WordDefinitionSentence.csv"
-# wordCovered="C:\\Users\\amukhopadhyay\\Downloads\\TrickyStuff\\PythonGrey\\WordsAlreadyCovered.csv"
-wordCovered="C:\\Users\\amukhopadhyay\\Desktop\\WordsAlreadyCovered.csv"
 q = Queue(maxsize=0)
 num_threads = 10
 ins = open( path, "r" )
